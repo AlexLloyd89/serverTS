9a278c3d3119de2c8d4a41b486ec8586ecef8c3c
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-07 07:47:22 -0400

    basic html inputs working & access to that data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log(`hi`);
+import express, { Request, Response } from "express";
+
+import bodyParser from "body-parser";
+
+import { router } from "./routes/loginRoutes";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log(`listening on port 3K`);
+});
