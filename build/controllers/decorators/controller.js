6bad4e520661e6a04105c47289005efaf660fba8
Alex Lloyd <lloyd.da89@gmail.com>
2019-09-08 08:57:01 -0400

    @use method working

--- build/controllers/decorators/controller.js
+++ build/controllers/decorators/controller.js
@@ -2,15 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 var appRouter_1 = require("../../appRouter");
+var MetadataKeys_1 = require("./MetadataKeys");
 function controller(routePrefix) {
     return function (target) {
         var router = appRouter_1.AppRouter.getInstance();
         for (var key in target.prototype) {
             var routeHandler = target.prototype[key];
-            var path = Reflect.getMetadata("path", target.prototype, key);
-            var method = Reflect.getMetadata("method", target.prototype, key);
+            var path = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.path, target.prototype, key);
+            var method = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.method, target.prototype, key);
+            var middlewares = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.middleware, target.prototype, key) ||
+                [];
             if (path) {
-                router[method](routePrefix + path, routeHandler);
+                router[method].apply(router, ["" + routePrefix + path].concat(middlewares, [routeHandler]));
             }
         }
     };
